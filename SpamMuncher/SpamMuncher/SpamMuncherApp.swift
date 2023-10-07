fdd1cf96bb237d67a8d36fd4129ddb01afe40fd6
Luka Sarcevic <lukasarcevic.business@gmail.com>
2023-10-07 23:22:51 +0200

    Initial commit
    Initial commit for SpamMuncher

---
+++ SpamMuncher/SpamMuncher/SpamMuncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpamMuncherApp.swift
+//  SpamMuncher
+//
+//  Created by Code Forge on 07.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct SpamMuncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
