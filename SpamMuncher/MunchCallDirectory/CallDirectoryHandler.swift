205838731b95165d0139d269e6fddf3ad5a77e0c
Luka Sarcevic <lukasarcevic.business@gmail.com>
2023-10-21 18:52:27 +0200

    Classes cleanup

--- SpamMuncher/MunchCallDirectory/CallDirectoryHandler.swift
+++ SpamMuncher/MunchCallDirectory/CallDirectoryHandler.swift
@@ -9,7 +9,6 @@ import Foundation
 import CallKit
 
 class CallDirectoryHandler: CXCallDirectoryProvider {
-
     override func beginRequest(with context: CXCallDirectoryExtensionContext) {
         context.delegate = self
 
@@ -103,13 +102,6 @@ class CallDirectoryHandler: CXCallDirectoryProvider {
 
 extension CallDirectoryHandler: CXCallDirectoryExtensionContextDelegate {
 
-    func requestFailed(for extensionContext: CXCallDirectoryExtensionContext, withError error: Error) {
-        // An error occurred while adding blocking or identification entries, check the NSError for details.
-        // For Call Directory error codes, see the CXErrorCodeCallDirectoryManagerError enum in <CallKit/CXError.h>.
-        //
-        // This may be used to store the error details in a location accessible by the extension's containing app, so that the
-        // app may be notified about errors which occurred while loading data even if the request to load data was initiated by
-        // the user in Settings instead of via the app itself.
-    }
+    func requestFailed(for extensionContext: CXCallDirectoryExtensionContext, withError error: Error) { }
 
 }
